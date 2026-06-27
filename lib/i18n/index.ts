c0cd070135ab0e55f6a66addcc1519ad3e3cb5b0
Berkin Sansal <berkinsansal@gmail.com>
2026-06-27 23:42:35 +0300

    Remove generated i18n lib files

--- lib/i18n/index.ts
+++
@@ -1,9 +0,0 @@
-import { tr } from './tr';
-import { en } from './en';
-
-export type Locale = 'tr' | 'en';
-export type Dict = typeof tr;
-
-export function getDict(locale: Locale): Dict {
-  return locale === 'en' ? en : tr;
-}
