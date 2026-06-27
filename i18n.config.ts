e951e9c0e0c95b0aed2c75db0326020b6e5dbc95
Berkin Sansal <berkinsansal@gmail.com>
2026-06-27 23:48:08 +0300

    Add i18n pathnames

--- i18n.config.ts
+++ i18n.config.ts
@@ -4,7 +4,26 @@ export const routing = defineRouting({
   locales: ['tr', 'en'],
   defaultLocale: 'tr',
   localePrefix: 'as-needed',
-  localeDetection: false
+  localeDetection: false,
+  pathnames: {
+    '/': '/',
+    '/about': {
+      tr: '/hakkimizda'
+    },
+    '/services': {
+      tr: '/hizmetler'
+    },
+    '/trainers': {
+      tr: '/egitmenler'
+    },
+    '/contact': {
+      tr: '/iletisim'
+    },
+    '/music': {
+      tr: '/muzik'
+    },
+    '/admin': '/admin'
+  }
 });
 
 export type Locale = (typeof routing.locales)[number];
