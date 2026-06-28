221dbc7e9c12f79042752c7ea5a8a21edab5f845
Berkin Sansal <berkinsansal@gmail.com>
2026-06-28 16:12:41 +0300

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -63,6 +63,7 @@ export default defineConfig([
           shorthandFirst: true,
           reservedFirst: true,
           ignoreCase: true,
+          noSortAlphabetically: true,
         },
       ],
 
